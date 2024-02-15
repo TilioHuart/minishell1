185a62e320a322e8a2b67b9fd1efd349190ba235
Tilio <tilio.huart@epitech.eu>
2024-02-15 16:23:45 +0100

    [UPDATE] delete dead code

--- src/main_functions/recup_function.c
+++ src/main_functions/recup_function.c
@@ -26,17 +26,12 @@ char **recup_function(void)
     char *buff = NULL;
     size_t len = 0;
     char **arr = NULL;
-    int count = 0;
 
     if (getline(&buff, &len, stdin) == -1) {
         perror("getline");
         return NULL;
     }
     remove_linebreak(buff);
-    for (size_t i = 0; buff[i] != '\0'; i += 1)
-        if ((buff[i] == ' ' && buff[i + 1] != ' ') ||
-            (buff[i] == ' ' && buff[i + 1] != '\0'))
-            count += 1;
     arr = my_str_to_word_array(buff);
     free(buff);
     return arr;
