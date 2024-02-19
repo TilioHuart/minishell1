ac288809e2313c010773f42c0d55a39f653657fb
Tilio <tilio.huart@epitech.eu>
2024-02-19 14:06:42 +0100

    [UPDATE] adding path in a array

--- src/path_functions/which_function.c
+++ src/path_functions/which_function.c
@@ -12,13 +12,60 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+static
+int retrieve_binary(char **arr, char **path_arr, char *path, size_t const *i)
+{
+    size_t len_path = 0;
+    size_t len_arr = 0;
+    size_t a = 0;
+
+    len_path = my_strlen(path_arr[*i]);
+    len_arr = my_strlen(arr[0]);
+    if (path != NULL)
+        free(path);
+    path = malloc(sizeof(char) * (len_arr + len_path + 1));
+    for (size_t y = 0; path_arr[*i][y] != '\0'; y++) {
+        path[a] = path_arr[*i][y];
+        a += 1;
+    }
+    path[a] = '/';
+    a += 1;
+    for (size_t y = 0; arr[0][y] != '\0'; y++) {
+        path[a] = arr[0][y];
+        a += 1;
+    }
+    path[a] = '\0';
+    a = 0;
+    printf("path1 = %s\n", path);
+    return SUCCESS;
+}
+
+static int concatenate_path(char **arr, char **path_arr)
+{
+    char *path = NULL;
+
+    if (arr == NULL || path_arr == NULL || arr[0] == NULL)
+        return FAILURE;
+    for (size_t i = 0; path_arr[i] != NULL; i++) {
+        retrieve_binary(arr, path_arr, path, &i);
+    }
+    return SUCCESS;
+}
+
 static int loop_in_path(char **arr, environment_t *environment)
 {
-    for (size_t i = 0; environment->value[i] != '\0'; i++) {
-        if (environment->value[i] == ':')
-            environment->value[i] = ' ';
+    char *path = NULL;
+    char **path_arr = NULL;
+
+    if (environment == NULL)
+        return FAILURE;
+    path = my_strdup(environment->value);
+    for (size_t i = 0; path[i] != '\0'; i++) {
+        if (path[i] == ':')
+            path[i] = ' ';
     }
-    printf("path = %s\n", environment->value);
+    path_arr = my_str_to_word_array(path);
+    concatenate_path(arr, path_arr);
     return SUCCESS;
 }
 
