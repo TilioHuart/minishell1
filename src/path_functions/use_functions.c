0270a875b8aa6e15374c15cdf88caa88087b1213
Tilio <tilio.huart@epitech.eu>
2024-02-21 15:17:57 +0100

    [UPDATE] execute file at root

--- src/path_functions/use_functions.c
+++ src/path_functions/use_functions.c
@@ -6,6 +6,7 @@
 */
 
 #include "environment.h"
+#include "my.h"
 #include "my_macros.h"
 #include "which_function.h"
 #include <sys/wait.h>
@@ -21,6 +22,26 @@ static void free_env(char ***env)
     free(*env);
 }
 
+static char *check_file_at_root(char **arr)
+{
+    if (arr == NULL || arr[0] == NULL)
+        return NULL;
+    if (my_strlen(arr[0]) < 3)
+        return NULL;
+    if (access(arr[0] + 2, F_OK | X_OK) == 0)
+        return my_strdup(arr[0] + 2);
+    return NULL;
+}
+
+static int file_at_root(char **path, char **arr)
+{
+    if (*path == NULL)
+        *path = check_file_at_root(arr);
+    if (*path == NULL)
+        return FAILURE;
+    return SUCCESS;
+}
+
 int use_function(char **arr, environment_t *environment)
 {
     char *path = NULL;
@@ -31,7 +52,7 @@ int use_function(char **arr, environment_t *environment)
     if (arr == NULL || arr[0] == NULL)
         return FAILURE;
     path = which_function(arr, environment);
-    if (path == NULL)
+    if (file_at_root(&path, arr) == FAILURE)
         return FAILURE;
     env = transform_env_to_arr(environment);
     pid = fork();
@@ -39,8 +60,8 @@ int use_function(char **arr, environment_t *environment)
         execve(path, arr, env);
     } else {
         waitpid(-1, &wstatus, 0);
+        free(path);
+        free_env(&env);
     }
-    free(path);
-    free_env(&env);
     return SUCCESS;
 }
