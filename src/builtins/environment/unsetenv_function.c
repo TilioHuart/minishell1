786671e07bf08b957c0d8f261aecd0966c6803f7
Tilio <tilio.huart@epitech.eu>
2024-02-23 10:19:16 +0100

    [UPDATE] add usetenv function

--- src/builtins/environment/unsetenv_function.c
+++ src/builtins/environment/unsetenv_function.c
@@ -5,12 +5,43 @@
 ** unsetenv_function.c
 */
 
+#include "my.h"
 #include "my_macros.h"
 #include "environment.h"
 #include <unistd.h>
+#include <stdlib.h>
+
+static
+void loop_in_env(char **arr, environment_t **environment, size_t const *i)
+{
+    environment_t *head = *environment;
+    environment_t *tmp = NULL;
+
+    while ((*environment)->next->next != NULL) {
+        if (my_strcmp((*environment)->next->key, arr[*i]) == 0)
+            break;
+        (*environment) = (*environment)->next;
+    }
+    if (my_strcmp((*environment)->next->key, arr[*i]) != 0 &&
+        (*environment)->next->next == NULL)
+        return;
+    tmp = (*environment)->next->next;
+    if ((*environment)->next->key != NULL)
+        free((*environment)->next->key);
+    if ((*environment)->next->value != NULL)
+        free((*environment)->next->value);
+    if ((*environment)->next != NULL)
+        free((*environment)->next);
+    (*environment)->next = tmp;
+    *environment = head;
+}
 
 int unsetenv_function(UNUSED char **arr, UNUSED environment_t *environment)
 {
-    write(1, "unsetenv", 8);
+    if (environment == NULL || arr == NULL || arr[0] == NULL)
+        return FAILURE;
+    for (size_t i = 1; arr[i] != NULL; i += 1) {
+        loop_in_env(arr, &environment, &i);
+    }
     return SUCCESS;
 }
