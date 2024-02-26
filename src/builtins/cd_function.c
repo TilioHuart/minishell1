fa48fb70f05f3c8185793de77c099397b4dc1473
Tilio <tilio.huart@epitech.eu>
2024-02-26 09:39:25 +0100

    [UPDATE] return 1 when cd fail

--- src/builtins/cd_function.c
+++ src/builtins/cd_function.c
@@ -5,12 +5,13 @@
 ** cd_function.c
 */
 
+#include "minishell.h"
 #include "my.h"
 #include "my_macros.h"
 #include "environment.h"
 #include <unistd.h>
 
-static int cd_home(environment_t *environment)
+static int cd_home(environment_t *environment, loop_t *loop)
 {
     while (environment->next != NULL) {
         if (my_strcmp(environment->key, "HOME") == 0)
@@ -23,30 +24,31 @@ static int cd_home(environment_t *environment)
         if (chdir(environment->value) == -1) {
             write(1, environment->key, my_strlen(environment->key));
             my_putstr(": No such file or directory.\n");
-            return 1;
+            loop->return_value = 1;
         }
     return SUCCESS;
 }
 
-int cd_function(UNUSED char **arr, UNUSED environment_t *environment)
+int cd_function(UNUSED char **arr, UNUSED environment_t *environment, loop_t *loop)
 {
     if (arr == NULL)
         return FAILURE;
     if (arr[1] == NULL) {
-        cd_home(environment);
-        return 1;
+        cd_home(environment, loop);
+        return SUCCESS;
     }
     for (int i = 0; arr[i] != NULL; i += 1) {
         if (i > 1) {
             write(1, arr[0], my_strlen(arr[0]));
             my_putstr(": Too many arguments.\n");
-            return 1;
+            loop->return_value = 1;
+            return SUCCESS;
         }
     }
     if (chdir(arr[1]) == -1) {
         write(1, arr[1], my_strlen(arr[1]));
         my_putstr(": No such file or directory.\n");
-        return 1;
+        loop->return_value = 1;
     }
     return SUCCESS;
 }
